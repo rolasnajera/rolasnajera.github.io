3f8e0c0eed98e23cc0eb4fb948ea9c8e061dfccb
Rolando Najera <me@rolasnajera.dev>
2024-09-15 13:33:19 +0200

    2 change docusaurus related content (#3)
    * Changing repository files to be ad-hoc
    
    * Changing all the docusaurus related content to be aligned with rolasnajera organization
    
    * Fixing social card name

---
+++ docs/projects/bio-links.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Bio links generation
+
+This projects is to create a bio website which contains personal links and social links.
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Learn by doing',
+    Svg: require('@site/static/img/learningByDoing.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        It's time to do the hard thing, we need to go beyond the tutorial hell. Let's learn by doing.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Share what you learn',
+    Svg: require('@site/static/img/sharing.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Beyond the knowledge you can acquire, one of the best options for deep learning is to teach.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Create community',
+    Svg: require('@site/static/img/community.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        We can grow faster together being part of the community and adding value to the organization.
       </>
     ),
   },
