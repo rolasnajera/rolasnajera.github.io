3aac8155b2058820be1817aca049118445f4b58e
Rolas Najera <me@rolasnajera.dev>
2024-09-17 10:41:21 +0200

    Including algolia configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -136,28 +136,14 @@ const config: Config = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Rolas Najera. Built with Docusaurus.`,
     },
-    /*algolia: {
-      // The application ID provided by Algolia
-      appId: 'YOUR_APP_ID',
-      // Public API key: it is safe to commit it
-      apiKey: 'YOUR_SEARCH_API_KEY',
-      indexName: 'YOUR_INDEX_NAME',
-      // Optional: see doc section below
+    algolia: {
+      appId: 'ZGAMIHYFGF',
+      apiKey: '7af360ab233d81d15d810239702b5f80',
+      indexName: 'rolasnajera',
       contextualSearch: true,
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: 'external\\.com|domain\\.com',
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      replaceSearchResultPathname: {
-        from: '/docs/', // or as RegExp: /\/docs\//
-        to: '/',
-      },
-      // Optional: Algolia search parameters
       searchParameters: {},
-      // Optional: path for search page that enabled by default (`false` to disable it)
       searchPagePath: 'search',
-      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-      insights: false,
-    },*/
+    },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
